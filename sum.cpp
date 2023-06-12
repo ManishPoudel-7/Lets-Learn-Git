e2c6bad9cc0b714841bd6bced5f99d26282ccfd9
Manish Poudel <manishpoudel2003@gmail.com>
2023-06-12 20:29:26 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a , b , sum =0;
+    cout<<"Enter the values:"<<endl;
+    cin>>a>>b;
+    sum = a + b;
+    cout<<"The sum is "<<sum<<endl;
+return 0;
+}
